d33e747fbf52610bd92ca128fb154ffddb421439
AiJiangnan <aijiangnan@webull.com>
2023-03-21 14:17:05 +0800

    Init repository.

---
+++ spring-cloud-commons/src/main/java/cn/codeartist/springcloud/commons/Main.java
@@ -0,0 +1,13 @@
+package cn.codeartist.springcloud.commons;
+
+/**
+ * ${Description}
+ *
+ * @author 艾江南
+ * @date ${DATE}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
